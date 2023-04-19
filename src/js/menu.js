d96ff86173dc3ba4e84ee58b9c7517b465000397
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-04-19 16:58:53 +0200

    Add menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,261 +1,385 @@
 
-export const HotPizzaMeals = (menu) => {
 
-  for (let el of menu) {
-    let menuListOfProducts = document.querySelector('.menu__listOfProducts-pizza');
+import Italiana from "../images/pizza-1.jpg";
+import Greak from "../images/pizza-2.jpg";
+import Caucasian from "../images/pizza-3.jpg";
+import American from "../images/pizza-4.jpg";
+import Tomatoe from "../images/pizza-5.jpg";
+import Margherita from "../images/pizza-6.jpg";
+import ItalianaBg from "../images/pizza-1-Bg.png";
+import HawaiaSpecialBg from "../images/pizza-5-Bg.png";
+import HawaiaBg from "../images/pizza-2-Bg.png";
+import Ultimae from "../images/pizza-7-Bg.png";
+import Bacon from "../images/pizza-8-Bg.png";
+import Crispy from "../images/pizza-4-Bg.png";
+import pricingImg from "../images/tre.png";
+import juicy from '../images/juicyMix.png';
+import cocaCola from '../images/juicyFruties.png';
+import sprite from '../images/juicyOrange.png';
+import burgerFirst from '../images/burger_1.png';
+import burgerSecond from '../images/burger_2.png';
+import burgerThird from '../images/burger_3.png';
+import pastaFirst from '../images/pasta_1.png';
+import pastaSecond from '../images/pasta_2.png';
+import pastaThird from '../images/pasta_3.png';
+import carT from "../images/bag.png";
+import about from "../images/about.jpg";
 
-    let divWrapperListProduct = document.createElement('div');
-    divWrapperListProduct.className = 'menu__listOfProducts-wrapper';
+import { HotPizzaMeals } from "./hotPizzaMeals";
+import { OurMenuPricing } from "./ourMenuPricing"
+import { HotPizzaMealsSecondRow } from './hotPizzaMeals';
 
-    let img = document.createElement('img');
+import { ChoosingTypeOfProduct } from "./typeOfProducts";
+import { ChangeBgForBtnCart } from "./cart";
+import { RemoveBgForBtnCart } from "./cart";
+import { AddSiblingElementToCart } from "./cart";
 
-    img.src = el.img;
-    menuListOfProducts.append(img);
-
-    let divWrapperDescription = document.createElement('div');
-    divWrapperDescription.className = 'desciption';
-
-    let headerDescription = document.createElement('h3');
-    headerDescription.className = "description__header";
-    headerDescription.textContent = el.name;
-
-    let descriptionText = document.createElement('p');
-    descriptionText.className = "description__text";
-    descriptionText.textContent = el.desciption;
-
-    let priceWrapper = document.createElement('div');
-    priceWrapper.className = "div__price-wrapper";
+import { AddSiblingElementToCartSecond } from "./cart";
 
 
-    let priceParagraph = document.createElement('p');
-    priceParagraph.textContent = el.price;
+let dataForSectionHotPizzaMeals = [
+  {
+      name: "Italiana Pizza",
+      desciption: "Far far away,behind the word mountains,far fram the countries Vakalia and Cansonantia",
+      img: Italiana,
+      price: "$2.00",
+  },
+  {
 
-    let btnOrder = document.createElement('button');
-    btnOrder.className = "btn-order";
-    btnOrder.textContent = "Order";
+      name: "Greak Pizza",
+      desciption: "Far far away,behind the word mountains,far fram the countries Vakalia and Cansonantia",
+      img: Greak,
+      price: "$2.30",
+  },
+  {
+
+      name: "Caucasian Pizza",
+      desciption: "Far far away,behind the word mountains,far fram the countries Vakalia and Cansonantia",
+      img: Caucasian,
+      price: "$2.45",
+  },
+];
+
+let dataForSectionHotPizzaMealsSecondRow = [
+  {
+      name: "American Pizza",
+      desciption: "a small river named Duden flows by their place and supplies",
+      img: American,
+      price: "$1.90",
+  },
+  {
+      name: "Tomatoe Pizza",
+      desciption: "a small river named Duden flows by their place and supplies",
+      img: Tomatoe,
+      price: "$2.20",
+  },
+  {
+      name: "Margherita Pizza",
+      desciption: "a small river named Duden flows by their place and supplies",
+      img: Margherita,
+      price: "$2.50",
+  },
+]
+
+let dataForMenuPricing = [
+  {
+      name: "Italiana Pizza",
+      desciption: "a small river named Duden flows by their place and supplies",
+      img: ItalianaBg,
+      price: "$29.00",
+  },
+  {
+
+      name: "Hawaia Special",
+      desciption: "a small river named Duden flows by their place and supplies",
+      img: HawaiaSpecialBg,
+      price: "$27.30",
+  },
+  {
+
+      name: "Caucasian Pizza",
+      desciption: "a small river named Duden flows by their place and supplies",
+      img: HawaiaBg,
+      price: "$21.45",
+  },
+  {
+      name: "American Pizza",
+      desciption: "a small river named Duden flows by their place and supplies",
+      img: Ultimae,
+      price: "$19.90",
+  },
+  {
+      name: "Tomatoe Pizza",
+      desciption: "a small river named Duden flows by their place and supplies",
+      img: Bacon,
+      price: "$30.20",
+  },
+  {
+      name: "Margherita Pizza",
+      desciption: "a small river named Duden flows by their place and supplies",
+      img: HawaiaBg,
+      price: "$21.50",
+  },
+  {
+      name: "Crispy Pizza",
+      desciption: "a small river named Duden flows by their place and supplies",
+      img: Crispy,
+      price: "$26.50",
+  },
+  {
+      name: "Crispy Pizza",
+      desciption: "a small river named Duden flows by their place and supplies",
+      img: Crispy,
+      price: "$22.50",
+  },
+]
+
+let dataPizzaList = [
+  {
+      name: "Hawaia Pizza",
+      desciption: "Far far away,behind the word mountains,far fram the countries Vakalia and Cansonantia",
+      img: HawaiaBg,
+      price: "$2.00",
+  },
+  {
+
+      name: "Greak Pizza",
+      desciption: "Far far away,behind the word mountains,far fram the countries Vakalia and Cansonantia",
+      img: HawaiaSpecialBg,
+      price: "$2.30",
+  },
+  {
+
+      name: "Caucasian Pizza",
+      desciption: "Far far away,behind the word mountains,far fram the countries Vakalia and Cansonantia",
+      img: Ultimae,
+      price: "$2.45",
+  },
+];
+let dataDrinksList = [
+  {
+      name: "Juice for Vitamin C",
+      desciption: "Far far away,behind the word mountains,far fram the countries Vakalia and Cansonantia",
+      img: juicy,
+      price: "$2.00",
+  },
+  {
+
+      name: "Pink or Grapefruit Juice",
+      desciption: "Far far away,behind the word mountains,far fram the countries Vakalia and Cansonantia",
+      img: cocaCola,
+      price: "$2.30",
+  },
+  {
+
+      name: "Low-Sodium Orange Juice",
+      desciption: "Far far away,behind the word mountains,far fram the countries Vakalia and Cansonantia",
+      img: sprite,
+      price: "$2.45",
+  },
+]
+let dataPastaList = [
+  {
+      name: "Homemade Pasta",
+      desciption: "Far far away,behind the word mountains,far fram the countries Vakalia and Cansonantia",
+      img: pastaFirst,
+      price: "$2.00",
+  },
+  {
+
+      name: "Pasta tres quesos",
+      desciption: "Far far away,behind the word mountains,far fram the countries Vakalia and Cansonantia",
+      img: pastaSecond,
+      price: "$2.30",
+  },
+  {
+
+      name: "Basic Semolina Pasta",
+      desciption: "Far far away,behind the word mountains,far fram the countries Vakalia and Cansonantia",
+      img: pastaThird,
+      price: "$2.45",
+  },
+]
+let dataBurgerList = [
+  {
+      name: "Burger Chicken ",
+      desciption: "Far far away,behind the word mountains,far fram the countries Vakalia and Cansonantia",
+      img: burgerFirst,
+      price: "$2.00",
+  },
+  {
+
+      name: "Burger Hot Spicy",
+      desciption: "Far far away,behind the word mountains,far fram the countries Vakalia and Cansonantia",
+      img: burgerSecond,
+      price: "$2.30",
+  },
+  {
+
+      name: "Hot Burger with pork",
+      desciption: "Far far away,behind the word mountains,far fram the countries Vakalia and Cansonantia",
+      img: burgerThird,
+      price: "$2.45",
+  },
+]
 
 
-    priceWrapper.append(priceParagraph);
-    priceWrapper.append(btnOrder);
+let pricingHeaderImg = document.querySelector('.menu-pricing-header__img');
+pricingHeaderImg.src = pricingImg;
 
-    divWrapperDescription.append(headerDescription);
-    divWrapperDescription.append(descriptionText);
-    divWrapperDescription.append(priceWrapper);
-    divWrapperListProduct.append(divWrapperDescription);
-    menuListOfProducts.append(divWrapperListProduct);
+let selectFoodImg = document.querySelector('.select-img');
+selectFoodImg.src = about;
+
+
+OurMenuPricing(dataForMenuPricing);
+
+HotPizzaMeals(dataForSectionHotPizzaMeals);
+
+HotPizzaMealsSecondRow(dataForSectionHotPizzaMealsSecondRow);
+
+ChoosingTypeOfProduct(dataPizzaList);
+
+
+
+let btnPizzaItems = document.querySelector('.link-pizza');
+let btnDrinksItems = document.querySelector('.link-two');
+let btnBurgersitems = document.querySelector('.link-burgers');
+let btmPastaItems = document.querySelector('.link-pasta');
+let ChooseTypeOfProductList = document.querySelector('.select-section-list__item');
+
+ChooseTypeOfProductList.addEventListener('click', function (e) {
+  switch (e.target) {
+      case btnPizzaItems:
+          ChangeBgForBtnCart(btnPizzaItems);
+          ChoosingTypeOfProduct(dataPizzaList);
+          RemoveBgForBtnCart(btnBurgersitems);
+          RemoveBgForBtnCart(btmPastaItems);
+          RemoveBgForBtnCart(btnDrinksItems);
+          break;
+      case btnDrinksItems:
+          ChangeBgForBtnCart(btnDrinksItems);
+          ChoosingTypeOfProduct(dataDrinksList);
+          RemoveBgForBtnCart(btnBurgersitems);
+          RemoveBgForBtnCart(btmPastaItems);
+          RemoveBgForBtnCart(btnPizzaItems);
+          break;
+      case btnBurgersitems:
+          ChangeBgForBtnCart(btnBurgersitems);
+          ChoosingTypeOfProduct(dataBurgerList);
+          RemoveBgForBtnCart(btnDrinksItems);
+          RemoveBgForBtnCart(btmPastaItems);
+          RemoveBgForBtnCart(btnPizzaItems);
+          break;
+      case btmPastaItems:
+          ChangeBgForBtnCart(btmPastaItems);
+          ChoosingTypeOfProduct(dataPastaList);
+          RemoveBgForBtnCart(btnDrinksItems);
+          RemoveBgForBtnCart(btnBurgersitems);
+          RemoveBgForBtnCart(btnPizzaItems);
+          break;
   }
-};
+});
+
+// cart
+
+let cartIcon = document.querySelector('.menu__cart img');
+cartIcon.src = carT;
 
 
-export const HotPizzaMealsSecondRow = (menu) => {
+let arrayForCartItems = [];
+if (arrayForCartItems.length > 3) {
+  arrayForCartItems.length = 0;
+}
 
-  for (let el of menu) {
-    let menuListOfProducts = document.querySelector('.menu__listOfProducts-pizza');
+window.addEventListener('click', (e) => {
 
-    let divWrapperListProduct = document.createElement('div');
-    divWrapperListProduct.className = 'menu__listOfProducts-wrapper';
-   
-    let imgPizza = document.createElement('img');
-    imgPizza.src = el.img;
+  let addToCartBtns = document.querySelectorAll(".menu-container-select-section__item-foods button");
+  for (let el of addToCartBtns) {
 
+      if (e.target == el) {
 
-    let divDescription = document.createElement('div');
-    divDescription.className = 'desciption';
+          let priceOfProducts = e.target.previousElementSibling;
+          let descriptionOfProducts = priceOfProducts.previousElementSibling;
+          let nameOfProducts = descriptionOfProducts.previousElementSibling;
 
-    let priceWrapper = document.createElement('div');
-    priceWrapper.className = "div__price-wrapper";
-
-    let headerDescription = document.createElement('h3');
-    headerDescription.className = "description__header";
-    headerDescription.textContent = el.name;
-
-    let descriptionText = document.createElement('p');
-    descriptionText.className = "description__text";
-    descriptionText.textContent = el.desciption;
-
-    let priceParagraph = document.createElement('p');
-    priceParagraph.textContent = el.price;
-
-    let btn = document.createElement('button');
-    btn.textContent = "Order";
+          let dataForCartItem = {
+              name: nameOfProducts.textContent,
+              price: priceOfProducts.textContent,
+          };
+          arrayForCartItems.length = 0;
+          arrayForCartItems.push(dataForCartItem);
+      }
 
 
-    priceWrapper.append(priceParagraph);
-    priceWrapper.append(btn);
-
-    divDescription.append(headerDescription);
-    divDescription.append(descriptionText);
-    divDescription.append(priceWrapper);
-    divWrapperListProduct.append(divDescription);
-    menuListOfProducts.append(divWrapperListProduct);
-    menuListOfProducts.append(imgPizza);
   }
-};
+})
 
-
-export const OurMenuPricing = (menu) => {
-
-  for (let el of menu) {
-    let divPricingMenu = document.querySelector('#pricing');
-
-    let divPricingContainer = document.createElement('div');
-    divPricingContainer.className = "div__container";
-
-    let divPricingItem = document.createElement('div');
-    divPricingItem.className = "menu-item";
-
-    let img = document.createElement('img');
-    img.src = el.img;
-
-    let nameOfPizza = document.createElement('p');
-    nameOfPizza.innerHTML = el.name;
-    nameOfPizza.className = "name__menu-pizza";
-
-    let priceOfPizza = document.createElement('p');
-    priceOfPizza.textContent = el.price;
-    priceOfPizza.className = "p__price";
-
-    let description = document.createElement('p');
-    description.textContent = el.desciption;
-    description.className = "description__pizza";
-
-    let divWrapperForImage = document.createElement('div');
-    divWrapperForImage.className = "wrapper__div-image";
-    let divWrapperDescription = document.createElement('div');
-
-    divWrapperForImage.append(nameOfPizza);
-    divWrapperForImage.append(priceOfPizza);
-
-    divWrapperDescription.append(description);
-    divPricingItem.append(img);
-    divWrapperForImage.append(divWrapperDescription);
-    divPricingItem.append(divWrapperForImage);
-    divPricingMenu.append(divPricingItem);
+window.addEventListener("click", function (e) {
+  if (e.target == cartIcon) {
+      document.querySelector("#my-modal").classList.add("open");
   }
-};
+})
+
+let listProduct = document.querySelector('.menu__listOfProducts-pizza');
+let btnOrder = document.querySelectorAll('.menu button');
+
+let btnAddToCart = document.querySelectorAll('.menu-container-select-section__item-foods button');
+let listProductSelectSection = document.querySelector('.menu-container-select-section__item-foods ');
 
 
-
-let count = 0;
-let counter = 0;
-
-export const ChoosingTypeOfProduct  = (foods) => {
-
-  let divContainerSelectFood = document.querySelector('.menu-container-select-section__item-foods');
-
-  for (let el of foods) {
-
-    let divItemsMenuWrapper = document.createElement('div');
-    divItemsMenuWrapper.className = "div__itemsMenu" + counter;
-    counter++;
-
-    if (counter >= 3) {
-      counter = 0;
-    }
-
-    let img = document.createElement('img');
-    img.src = el.img;
-
-    let nameOfPizza = document.createElement('p');
-    nameOfPizza.textContent = el.name;
-    nameOfPizza.className = "name-pizza";
-
-    let divWrapperDescription = document.createElement('div');
-
-    let desciptionOfPizza = document.createElement('p');
-    desciptionOfPizza.textContent = el.desciption;
-    desciptionOfPizza.className = "description-pizza";
-
-    let priceOfPizza = document.createElement('p');
-    priceOfPizza.textContent = el.price;
-    priceOfPizza.className = "pizza-price";
-
-    let btnAddToCart = document.createElement('button');
-
-    btnAddToCart.textContent = "Add to cart";
-    btnAddToCart.className = 'btn__addToCart' + count;
-    count++;
-
-    if (count >= 3) {
-      count = 0;
-    }
-
-    divItemsMenuWrapper.append(img);
-    divItemsMenuWrapper.append(nameOfPizza);
-    divWrapperDescription.append(desciptionOfPizza);
-    divItemsMenuWrapper.append(divWrapperDescription);
-    divItemsMenuWrapper.append(priceOfPizza);
-    divItemsMenuWrapper.append(btnAddToCart);
-    divContainerSelectFood.append(divItemsMenuWrapper);
+listProduct.addEventListener('click', (e) => {
+  for (let el of btnOrder) {
+      if (e.target == el) {
+          AddSiblingElementToCart(e.target);
+      }
   }
-};
 
-export const AddSiblingElementToCart = (event) => {
- 
-  let sibling = event.parentElement;
+});
 
-  let parent = sibling.previousElementSibling;
-
-  let name = parent.previousElementSibling;
- 
-  let price = event.previousElementSibling;
-
- 
-  let modalBox = document.querySelector('.modal__box-products');
-  let nameOfProducts = document.createElement('p');
-  nameOfProducts.textContent = name.textContent;
-
-  let parPrice = document.createElement('p');
-  parPrice.textContent = price.textContent;
-  
-  modalBox.append(nameOfProducts);
-  modalBox.append(parPrice);
-};
-export const AddSiblingElementToCartSecond = (event) => {
-
-  let price = event.previousElementSibling;
-  
-  let description = price.previousElementSibling;
-  let name = description.previousElementSibling;
- 
-
-
- 
-  let modalBox = document.querySelector('.modal__box-products');
-  let nameOfProducts = document.createElement('p');
-  nameOfProducts.textContent = name.textContent;
-
-  let parPrice = document.createElement('p');
-  parPrice.textContent = price.textContent;
-  
-  modalBox.append(nameOfProducts);
-  modalBox.append(parPrice);
-};
-
-export const ChangeBgForBtnCart = (element) => {
-
-  isChildren();
-  element.style.color = "black";
-  element.style.backgroundColor = 'brown';
-  element.style.border = 'none';
-
-};
-
-
-const isChildren = () => {
-  let div = document.querySelector('.menu-container-select-section__item-foods');
-
-  if (div.children) {
-    div.innerHTML = "";
+listProductSelectSection.addEventListener('click', (e) => {
+  for (let el of btnAddToCart) {
+      if (e.target == el) {
+          AddSiblingElementToCartSecond(e.target);
+      }
   }
-};
+})
 
-export const RemoveBgForBtnCart = (element) => {
 
-  element.style.color = "rgb(243, 135, 12)";
-  element.style.backgroundColor = "";
-  element.style.border = "1px solid rgb(243, 135, 12)";
-};
+let buttonClosePopup = document.querySelector('#close-my-modal-btn svg');
+
+// modal window
+window.addEventListener("click", function (e) {
+
+  if (e.target == buttonClosePopup) {
+
+      document.querySelector("#my-modal").classList.remove("open");
+
+  }
+})
+
+// close modal window , press Esc
+window.addEventListener('keydown', (e) => {
+  if (e.key === "Escape") {
+      document.getElementById("my-modal").classList.remove("open");
+  }
+});
+
+//  Close modal window , click outside
+document.querySelector("#my-modal .menu-cart-modal__box").addEventListener('click', event => {
+  event._isClickWithInModal = true;
+});
+// remove class
+document.getElementById("my-modal").addEventListener('click', event => {
+  if (event._isClickWithInModal) return;
+  event.currentTarget.classList.remove('open');
+});
+
+
+
+
+
+
+
 
 
 
