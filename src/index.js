0952a3c62b3f7e6b49cfca26a8c300a366d0f98a
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-04-20 12:26:20 +0200

    update menu_section

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 
-import '@/styles/index.scss'
-import '@/js/header/slider'
+import '@/styles/index.scss';
+import '@/js/menu/menu.js';
+import '@/js/menu/cart.js';
+import '@/js/menu/hotPizzaMeals.js';
+import '@/js/menu/ourMenuPricing';
+import '@/js/menu/typeOfProducts';
+
+
+
