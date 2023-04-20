402dfa2b844ec1fb4776b269a817e3c7bf8b4b0a
Kateryna Laina <boba4ok@gmail.com>
2023-04-20 15:55:21 +0300

    burger menu added

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -31,7 +31,23 @@ if (isMobile.any()) {
     document.body.classList.add('_pc')
 }
 
-//прокрутка при клике
+
+//burger menu
+
+const iconMenu = document.querySelector('.menu__icon')
+const menuBody = document.querySelector('.menu')
+
+if (iconMenu) {
+
+    iconMenu.addEventListener('click', (e) => {
+        document.body.classList.toggle('_lock')
+        iconMenu.classList.toggle('_active')
+        menuBody.classList.toggle('_active')
+    })
+}
+
+
+//scroll
 
 const menuLinks = document.querySelectorAll('li[data-goto]')
 
@@ -42,6 +58,11 @@ if (menuLinks.length > 0) {
             if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
                 const gotoBlock = document.querySelector(menuLink.dataset.goto)
                 const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight
+                if(iconMenu.classList.contains('_active')){
+                    document.body.classList.remove('_lock')
+                    iconMenu.classList.remove('_active')
+                    menuBody.classList.remove('_active')
+                }
 
                 window.scrollTo({
                     top: gotoBlockValue,
@@ -52,3 +73,7 @@ if (menuLinks.length > 0) {
         })
     })
 }
+
+
+
+
