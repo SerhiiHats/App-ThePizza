26e01c13db5b36f64525106e22dcfd56d8a96080
Kateryna Laina <boba4ok@gmail.com>
2023-04-20 17:28:30 +0300

    adaptive design finished

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -1,36 +1,3 @@
-const isMobile = {
-    Android: function () {
-        return navigator.userAgent.match(/Android/i)
-    },
-    BlackBerry: function () {
-        return navigator.userAgent.match(/BlackBerry/i)
-    },
-    iOS: function () {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i)
-    },
-    Opera: function () {
-        return navigator.userAgent.match(/Opera Mini/i)
-    },
-    Windows: function () {
-        return navigator.userAgent.match(/IEMobile/i)
-    },
-    any: function () {
-        return (
-            isMobile.Android() ||
-            isMobile.BlackBerry() ||
-            isMobile.iOS() ||
-            isMobile.Opera() ||
-            isMobile.Windows()
-        )
-    }
-}
-
-if (isMobile.any()) {
-    document.body.classList.add('_touch')
-} else {
-    document.body.classList.add('_pc')
-}
-
 
 //burger menu
 
@@ -49,29 +16,34 @@ if (iconMenu) {
 
 //scroll
 
-const menuLinks = document.querySelectorAll('li[data-goto]')
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]')
 
 if (menuLinks.length > 0) {
-    menuLinks.forEach(element => {
-        element.addEventListener('click', (e) => {
-            let menuLink = e.target
-            if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-                const gotoBlock = document.querySelector(menuLink.dataset.goto)
-                const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight
-                if(iconMenu.classList.contains('_active')){
-                    document.body.classList.remove('_lock')
-                    iconMenu.classList.remove('_active')
-                    menuBody.classList.remove('_active')
-                }
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', onMenuLinkClick)
 
-                window.scrollTo({
-                    top: gotoBlockValue,
-                    behavior: 'smooth'
-                })
-                e.preventDefault()
+            function onMenuLinkClick(e){
+
+                let menuLink = e.target
+                if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+                    const gotoBlock = document.querySelector(menuLink.dataset.goto)
+                    const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight
+                    if(iconMenu.classList.contains('_active')){
+                        document.body.classList.remove('_lock')
+                        iconMenu.classList.remove('_active')
+                        menuBody.classList.remove('_active')
+                    }
+
+                    window.scrollTo({
+                        top: gotoBlockValue,
+                        behavior: 'smooth'
+                    })
+                    e.preventDefault()
+                }
             }
+
+
         })
-    })
 }
 
 
