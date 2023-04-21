3feb4bc23f2a51dce4aaad5ad34110746b1482ff
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-04-21 13:56:47 +0200

    delete hotPizzaMeals.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import '@/js/header/slider';
 
 import '@/js/menu/menu.js';
 import '@/js/menu/cart.js';
-import '@/js/menu/hotPizzaMeals.js';
 import '@/js/menu/ourMenuPricing';
 import '@/js/menu/typeOfProducts';
 
